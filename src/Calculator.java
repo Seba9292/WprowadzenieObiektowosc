1666ba8ebf7ab16d3ac733ce24dd9ddfd91dba9c
Seba9292 <sebaszym92@gmail.com>
2022-07-24 11:12:13 +0200

    Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+/*
+1.Dodawanie
+2.Mnozenie
+3.Odejmowanie
+ */
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car ford = new Car("Black", 5, "Puma", true);
-        System.out.println(ford.getautomaticGearbox());
-       ford.setAutomaticGearbox(false);
-        System.out.println(ford.getautomaticGearbox());
+/*
+        int a;
+        int b;
+        a = 10;
+        b = 2;
+        int c;
 
-        Car suzuki = new Car("Blue",3, "Jimny", true);
-        System.out.println(suzuki.getColour());
-        suzuki.setColour("Green");
-        System.out.println(suzuki.getColour());
+
+        c = a - b;
+
+        System.out.println(c);
+
+ */
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Prosze podac pierwsza liczbe");
+        int firstVariable = scanner.nextInt();
+        System.out.println("Prosze podac druga liczbe");
+        int secondVariable = scanner.nextInt();
+        System.out.println("Twoj wynik dodawania to " + calculator.add(firstVariable, secondVariable));
+
+        System.out.println("Prosze podac pierwsza liczbe");
+        firstVariable = scanner.nextInt();
+        System.out.println("Prosze podac druga liczbe");
+        secondVariable = scanner.nextInt();
+        System.out.println("Twoj wynik odejmowania to " + calculator.sub(firstVariable, secondVariable));
+
+        System.out.println("Prosze podac pierwsza liczbe");
+        firstVariable = scanner.nextInt();
+        System.out.println("Prosze podac druga liczbe");
+        secondVariable = scanner.nextInt();
+        System.out.println("Twoj wynik odejmowania to " + calculator.multiply(firstVariable, secondVariable));
 
     }
+
+
 }
+
